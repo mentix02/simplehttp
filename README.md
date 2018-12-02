f2de33a1c14125e059418437d36f49a370513b10
mentix02 <manan.yadav02@gmail.com>
2018-12-03 04:53:51 +0530

    made port value default to 8000 and updated README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # simplehttp
 a clone of Python's SimpleHTTPServer written in Go
+
+## usage
+```bash
+go get github.com/mentix02/simplehttp
+simplehttp <port>
+```
+
+## info
+port is optional with the default value being 8000
+$GOPATH/bin should be in default PATH
+
+## credits
+inspired by [mubaris' clone](https://github.com/mubaris/simpleHTTPServer)
--- simplehttp.go
+++ simplehttp.go
@@ -10,27 +10,28 @@ import (
 
 func main() {
 
-	usage := "usage: " + os.Args[0] + " <port>" // usage message for errors
-
 	args := os.Args // commandline arguments
 
-	if len(args) != 2 { // checking for only 2 arguments provided
+	http.Handle("/", http.FileServer(http.Dir("."))) // handler for handler for server
 
-		fmt.Println(usage) // print usage error
-		os.Exit(1)
+	if len(args) == 1 { // check for port number
+
+		go fmt.Println("Serving HTTP on 0.0.0.0 port 8000 (http://0.0.0.0:8000/) ...")
+		log.Fatal(http.ListenAndServe(":8000", nil))
+		os.Exit(0)
 
 	} else {
 
 		port, err := strconv.Atoi(args[1]) // checking if port provided is a number
 		if err != nil {
 
-			fmt.Println(usage) // print usage error
+			fmt.Println("usage: " + os.Args[0] + " <port>") // print usage error
 			os.Exit(1)
 
 		} else {
 
-			http.Handle("/", http.FileServer(http.Dir("."))) // handler for handler for server
-			go fmt.Printf("Serving HTTP on 0.0.0.0 port %d (http://0.0.0.0:%d/) ...\n", port, port) // concurrent message for starting server
+			// concurrent message for starting server
+			go fmt.Printf("Serving HTTP on 0.0.0.0 port %d (http://0.0.0.0:%d/) ...\n", port, port)
 			log.Fatal(http.ListenAndServe(":"+os.Args[1], nil))
 
 		}
